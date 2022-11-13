05b286872062fe914c8773e8fbbb2af02c517cf7
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2022-11-14 00:31:12 +0500

    Add finished first version of lw

--- main.cpp
+++ main.cpp
@@ -5,216 +5,206 @@ using namespace std;
 
 
 class Point2D {
-protected:
+private:
     double x, y;
 
 public:
     Point2D() {
-        cout << "Point2D: Constructor" << endl;
+        cout << "Point2D:\t Point2D()" << endl;
         x = 0;
         y = 0;
     }
 
     Point2D(double set_x, double set_y) {
-        cout << "Point2D: Constructor" << endl;
+        cout << "Point2D:\t Point2D(double set_x, double set_y)" << endl;
         x = set_x;
         y = set_y;
     }
 
     Point2D(Point2D const &point2D) {
-        cout << "Point2D: Constructor" << endl;
+        cout << "Point2D:\t Point2D(Point2D const &point2D)" << endl;
         x = point2D.x;
         y = point2D.y;
     }
 
     ~Point2D() {
-        cout << "Point2D: Destructor" << endl;
+        cout << "Point2D:\t ~Point2D()" << endl;
     }
 
-    double GetX() {
-        cout << "Point2D: GetX" << endl;
+    double GetX() const {
+        cout << "Point2D:\t double GetX()" << endl;
         return x;
     }
 
-    double GetY() {
-        cout << "Point2D: GetY" << endl;
+    double GetY() const {
+        cout << "Point2D:\t double GetY()" << endl;
         return y;
     }
 
     void SetX(double set_x) {
-        cout << "Point2D: SetX" << endl;
+        cout << "Point2D:\t void SetX(double set_x)" << endl;
         x = set_x;
     }
 
     void SetY(double set_y) {
-        cout << "Point2D: SetY" << endl;
+        cout << "Point2D:\t void SetY(double set_y)" << endl;
         y = set_y;
     }
-};
 
-class Point3D : public Point2D {
-private:
-    double z;
-
-public:
-    Point3D() : Point2D() {
-        cout << "Point3D: Constructor" << endl;
-        z = 0;
-    }
-
-    Point3D(Point3D const &point3D) {
-        cout << "Point3D: Constructor" << endl;
-        x = point3D.x;
-        y = point3D.y;
-    }
-
-    Point3D(double set_x, double set_y, double set_z) : Point2D(set_x, set_y) {
-        cout << "Point3D: Constructor" << endl;
-        z = set_z;
-    }
-
-    Point3D(Point2D set_point2D, double setY, double setZ) : Point2D(set_point2D.GetX(), set_point2D.GetY()) {
-        cout << "Point3D: Constructor" << endl;
-        z = setZ;
-    }
-
-    ~Point3D() {
-        cout << "Point3D: Destructor" << endl;
-    }
-
-    double GetZ() {
-        cout << "Point3D: GetZ" << endl;
-        return z;
-    }
-
-    void SetZ(double set_z) {
-        cout << "Point3D: SetZ" << endl;
-        z = set_z;
+    void SetCoordinates(double set_x, double set_y) {
+        cout << "Point2D:\t void SetCoordinates(double set_x, double set_y)" << endl;
+        x = set_x;
+        y = set_y;
     }
 };
 
 class Figure2D {
 public:
-    virtual double GetSquare() = 0;
+    Figure2D() {
+        cout << "Figure2D:\t Figure2D()" << endl;
+    }
 
-    virtual double GetPerimeter() = 0;
+    virtual ~Figure2D() {
+        cout << "Figure2D:\t ~Figure2D()" << endl;
+    }
+
+    virtual double GetSquare() {
+        cout << "Figure2D:\t double GetSquare()" << endl;
+        return -1;
+    }
+
+    virtual double GetPerimeter() {
+        cout << "Figure2D:\t double GetPerimeter()" << endl;
+        return -1;
+    }
 };
 
-class Rectangle : Figure2D {
+class Rectangle : public Figure2D {
 protected:
     Point2D first_point, second_point;
 
-private:
     double CalculateSideA() {
-        cout << "Rectangle: CalculateSideA" << endl;
+        cout << "Rectangle:\t double CalculateSideA()" << endl;
         return abs(first_point.GetX() - second_point.GetX());
     }
 
     double CalculateSideB() {
-        cout << "Rectangle: CalculateSideB" << endl;
+        cout << "Rectangle:\t double CalculateSideB()" << endl;
         return abs(first_point.GetY() - second_point.GetY());
     }
 
 public:
     Rectangle(Rectangle const &rectangle) {
-        cout << "Rectangle: Constructor" << endl;
+        cout << "Rectangle:\t Rectangle(Rectangle const &rectangle)" << endl;
         first_point = rectangle.first_point;
         second_point = rectangle.second_point;
     }
 
-    Rectangle(Point2D set_first_point, Point2D set_second_point) {
-        cout << "Rectangle: Constructor" << endl;
+    Rectangle(const Point2D &set_first_point, const Point2D &set_second_point) {
+        cout << "Rectangle:\t Rectangle(const Point2D &set_first_point, const Point2D &set_second_point)" << endl;
         first_point = set_first_point;
         second_point = set_second_point;
     }
 
-    Rectangle(double first_point_x, double first_point_y, double second_point_x, double second_point_y) {
-        cout << "Rectangle: Constructor" << endl;
-        first_point.SetX(first_point_x);
-        first_point.SetY(first_point_y);
-        second_point.SetX(second_point_x);
-        second_point.SetY(second_point_y);
+    Rectangle(const Point2D &set_left_down_point, double side_a, double side_b) {
+        cout << "Rectangle:\t Rectangle(const Point2D &set_left_down_point, double side_a, double side_b)" << endl;
+        first_point = set_left_down_point;
+        second_point.SetCoordinates(set_left_down_point.GetX() + side_a, set_left_down_point.GetY() + side_b);
     }
 
-    ~Rectangle() {
-        cout << "Rectangle: Destructor" << endl;
+    Rectangle(double first_point_x, double first_point_y, double second_point_x, double second_point_y) {
+        cout
+                << "Rectangle:\t Rectangle(double first_point_x, double first_point_y, double second_point_x, double second_point_y)"
+                << endl;
+        first_point.SetCoordinates(first_point_x, first_point_y);
+        second_point.SetCoordinates(second_point_x, second_point_y);
+    }
+
+    Rectangle() {
+        cout << "Rectangle:\t Rectangle()" << endl;
+        first_point.SetCoordinates(0, 0);
+        second_point.SetCoordinates(0, 0);
+    }
+
+    ~Rectangle() override {
+        cout << "Rectangle:\t ~Rectangle()" << endl;
     }
 
     Point2D GetFirstPoint() {
-        cout << "Rectangle: GetFirstPoint" << endl;
+        cout << "Rectangle:\t Point2D GetFirstPoint()" << endl;
         return first_point;
     }
 
     Point2D GetSecondPoint() {
-        cout << "Rectangle: GetSecondPoint" << endl;
+        cout << "Rectangle:\t Point2D GetSecondPoint()" << endl;
         return second_point;
     }
 
-    void SetFirstPoint(Point2D set_point2D) {
-        cout << "Rectangle: SetFirstPoint" << endl;
+    void SetFirstPoint(const Point2D &set_point2D) {
+        cout << "Rectangle:\t void SetFirstPoint(const Point2D &set_point2D)" << endl;
         first_point = set_point2D;
     }
 
-    void SetSecondPoint(Point2D set_point2D) {
-        cout << "Rectangle: SetSecondPoint" << endl;
+    void SetSecondPoint(const Point2D &set_point2D) {
+        cout << "Rectangle:\t void SetSecondPoint(const Point2D &set_point2D)" << endl;
         second_point = set_point2D;
     }
 
     double GetPerimeter() override {
-        cout << "Rectangle: GetPerimeter" << endl;
+        cout << "Rectangle:\t double GetPerimeter()" << endl;
         return (CalculateSideA() + CalculateSideB()) * 2;
     }
 
     double GetSquare() override {
-        cout << "Rectangle: GetSquare" << endl;
+        cout << "Rectangle:\t double GetSquare()" << endl;
         return CalculateSideA() * CalculateSideB();
     }
 };
 
 class Square : public Rectangle {
 private:
-    double CalculateSide() {
-        cout << "Square: CalculateSide" << endl;
-        return abs(first_point.GetY() - second_point.GetY());
-    }
+    using Rectangle::SetSecondPoint;
 
 public:
-    Square(Square const &square) {
+    Square(Square const &square) : Rectangle(square) {
+        cout << "Square:\t Square(Square const &square) : Rectangle(square)" << endl;
         first_point = square.first_point;
         second_point = square.second_point;
     }
 
-    Square(Point2D set_first_point, Point2D set_second_point) : Rectangle(set_first_point, set_second_point) {
-
+    Square(const Point2D &set_first_point, double side_length) : Rectangle(set_first_point.GetX(),
+                                                                           set_first_point.GetY(),
+                                                                           set_first_point.GetX() + side_length,
+                                                                           set_first_point.GetY() + side_length) {
+        cout
+                << "Square:\t Square(const Point2D &set_first_point, double side_length) : Rectangle(set_first_point.GetX(), set_first_point.GetY(), set_first_point.GetX() + side_length, set_first_point.GetY() + side_length)"
+                << endl;
     };
 
-    Square(double first_point_x, double first_point_y, double second_point_x, double second_point_y) : Rectangle(
-            first_point_x, first_point_y, second_point_x, second_point_y) {
-
+    Square(double first_point_x, double first_point_y, double side_length) : Rectangle(
+            first_point_x, first_point_y, first_point_x + side_length, first_point_y + side_length) {
+        cout
+                << "Square:\t Square(double first_point_x, double first_point_y, double side_length) : Rectangle(first_point_x, first_point_y, first_point_x + side_length, first_point_y + side_length)"
+                << endl;
     };
 
-    Point2D GetFirstPoint() {
-        return first_point;
+    Square() : Rectangle() {
+        cout << "Square:\t Square() : Rectangle()" << endl;
     }
 
-    Point2D GetSecondPoint() {
-        return second_point;
+
+    ~Square() override {
+        cout << "Square:\t ~Square()" << endl;
     }
 
-    void SetFirstPoint(Point2D set_point2D) {
-        first_point = set_point2D;
-    }
-
-    void SetSecondPoint(Point2D set_point2D) {
-        second_point = set_point2D;
+    void SetSideLength(double side_length) {
+        cout << "Square:\t void SetSideLength(double side_length)" << endl;
+        second_point.SetCoordinates(first_point.GetX() + side_length, first_point.GetY() + side_length);
     }
 
     double GetPerimeter() override {
-        return CalculateSide() * 4;
-    }
-
-    double GetSquare() override {
-        return CalculateSide() * CalculateSide();
+        cout << "Square:\t double GetPerimeter()" << endl;
+        return CalculateSideA() * 4;
     }
 };
 
@@ -222,7 +212,10 @@ class Triangle : public Figure2D {
 private:
     Point2D first_point, second_point, third_point;
 
-    static double CalculateSide(Point2D first_side_point, Point2D second_side_point) {
+    static double CalculateSide(const Point2D &first_side_point, const Point2D &second_side_point) {
+        cout
+                << "Triangle:\t static double CalculateSide(const Point2D &first_side_point, const Point2D &second_side_point)"
+                << endl;
         return sqrt(
                 (second_side_point.GetX() - first_side_point.GetX()) *
                 (second_side_point.GetX() - first_side_point.GetX()) +
@@ -231,59 +224,171 @@ private:
     }
 
     double CalculateSideA() {
+        cout << "Triangle:\t double CalculateSideA()" << endl;
         return CalculateSide(first_point, second_point);
     }
 
     double CalculateSideB() {
+        cout << "Triangle:\t double CalculateSideB()" << endl;
         return CalculateSide(first_point, third_point);
     }
 
     double CalculateSideC() {
+        cout << "Triangle:\t double CalculateSideC()" << endl;
         return CalculateSide(third_point, second_point);
     }
 
+
 public:
+    Triangle(const Point2D &set_first_point, const Point2D &set_second_point, const Point2D &set_third_point) {
+        cout
+                << "Triangle:\t Triangle(const Point2D &set_first_point, const Point2D &set_second_point, const Point2D &set_third_point)"
+                << endl;
+        first_point = set_first_point;
+        second_point = set_second_point;
+        third_point = set_third_point;
+    }
+
+    Triangle(const double &set_first_point_x, const double &set_first_point_y, const double &set_second_point_x,
+             const double &set_second_point_y, const double &set_third_point_x, const double &set_third_point_y) {
+        cout
+                << "Triangle:\t Triangle(const double &set_first_point_x, const double &set_first_point_y, const double &set_second_point_x, const double &set_second_point_y, const double &set_third_point_x, const double &set_third_point_y) "
+                << endl;
+        first_point.SetCoordinates(set_first_point_x, set_first_point_y);
+        second_point.SetCoordinates(set_second_point_x, set_second_point_y);
+        third_point.SetCoordinates(set_third_point_x, set_third_point_y);
+    }
+
+    Triangle(const Triangle &triangle) {
+        cout << "Triangle:\t Triangle(const Triangle &triangle)" << endl;
+        first_point = triangle.first_point;
+        second_point = triangle.second_point;
+        third_point = triangle.third_point;
+    }
+
+    Triangle() {
+        cout << "Triangle:\t Triangle()" << endl;
+        first_point.SetCoordinates(0, 0);
+        second_point.SetCoordinates(0, 0);
+        third_point.SetCoordinates(0, 0);
+    }
+
+    ~Triangle() override {
+        cout << "Triangle:\t ~Triangle()" << endl;
+    }
+
     Point2D GetFirstPoint() {
+        cout << "Triangle:\t Point2D GetFirstPoint()" << endl;
         return first_point;
     }
 
     Point2D GetSecondPoint() {
+        cout << "Triangle:\t Point2D GetSecondPoint()" << endl;
         return second_point;
     }
 
     Point2D GetThirdPoint() {
+        cout << "Triangle:\t Point2D GetThirdPoint()" << endl;
         return second_point;
     }
 
-    void SetFirstPoint(Point2D setPoint2D) {
+    void SetFirstPoint(const Point2D &setPoint2D) {
+        cout << "Triangle:\t void SetFirstPoint(const Point2D &setPoint2D)" << endl;
         first_point = setPoint2D;
     }
 
-    void SetSecondPoint(Point2D setPoint2D) {
+    void SetSecondPoint(const Point2D &setPoint2D) {
+        cout << "Triangle:\t void SetSecondPoint(const Point2D &setPoint2D)" << endl;
         second_point = setPoint2D;
     }
 
-    void SetThirdPoint(Point2D setPoint2D) {
+    void SetThirdPoint(const Point2D &setPoint2D) {
+        cout << "Triangle:\t void SetThirdPoint(const Point2D &setPoint2D)" << endl;
         first_point = setPoint2D;
     }
 
     double GetPerimeter() override {
+        cout << "Triangle:\t double GetPerimeter()" << endl;
         return CalculateSideA() + CalculateSideB() + CalculateSideC();
     }
 
     double GetSquare() override {
+        cout << "Triangle:\t double GetSquare()" << endl;
         double halfPerimeter = GetPerimeter() / 2;
         return sqrt(halfPerimeter * (halfPerimeter - CalculateSideA()) * (halfPerimeter - CalculateSideB()) *
                     (halfPerimeter - CalculateSideC()));
     }
 };
 
-class Figure3D {
-public:
-    virtual double GetVolume() = 0;
-};
-
-
 int main() {
+    double tmp_val;
+    Point2D pointA = Point2D(0, 0);
+    Point2D pointB = Point2D(5, 5);
+    Point2D pointC = Point2D(10, 0);
+    cout << "==============================" << endl << endl;
+
+    Figure2D **figures = new Figure2D *[3];
+    cout << "----------" << endl;
+    figures[0] = new Rectangle(pointA, pointB);
+    cout << "----------" << endl;
+    figures[1] = new Square(pointA, 25);
+    cout << "----------" << endl;
+    figures[2] = new Triangle(pointA, pointB, pointC);
+    cout << "----------" << endl;
+
+    for (int i = 0; i < 3; i++) {
+        tmp_val = figures[i]->GetPerimeter();
+        cout << "Calculated perimeter: " << tmp_val << endl << endl;
+        cout << "----------" << endl;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        delete figures[i];
+        cout << "----------" << endl;
+    }
+    cout << "==============================" << endl << endl;
+
+    Rectangle rectangle = Rectangle(pointA, pointB);
+    cout << "----------" << endl;
+    tmp_val = rectangle.GetSquare();
+    cout << "Calculated square: " << tmp_val << endl;
+    cout << "==============================" << endl << endl;
+
+    Square square = Square(pointA, 25);
+    cout << "----------" << endl;
+    tmp_val = square.GetSquare();
+    cout << "Calculated square: " << tmp_val << endl;
+    cout << "----------" << endl;
+    tmp_val = square.GetFirstPoint().GetX();
+    cout << "Get x: " << tmp_val << endl;
+    cout << "----------" << endl;
+    square.SetSideLength(1);
+    cout << "----------" << endl;
+    tmp_val = square.GetSquare();
+    cout << "Calculated square: " << tmp_val << endl;
+    cout << "==============================" << endl << endl;
+
+    Rectangle square_rectangle = Square(pointA, 25);
+    cout << "----------" << endl;
+    tmp_val = square_rectangle.GetSquare();
+    cout << "Calculated square: " << tmp_val << endl;
+    cout << "==============================" << endl << endl;
+
+    Square square_empty;
+    cout << "----------" << endl;
+    tmp_val = square_empty.GetSquare();
+    cout << "Calculated square: " << tmp_val << endl;
+    cout << "----------" << endl;
+    square_empty.SetSideLength(8);
+    cout << "----------" << endl;
+    tmp_val = square_empty.GetSquare();
+    cout << "Calculated square: " << tmp_val << endl;
+    cout << "----------" << endl;
+    square_empty = Square(0, 0, 3);
+    cout << "----------" << endl;
+    tmp_val = square_empty.GetSquare();
+    cout << "Calculated square: " << tmp_val << endl;
+    cout << "==============================" << endl << endl;
+
     return 0;
 }
